3c64602885877be744b2c8d5d72e22a909317eac
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2022-01-22 22:56:15 -0500

    Append y preppend

--- LinkedList/SingleLinkedList/Single.js
+++ LinkedList/SingleLinkedList/Single.js
@@ -1,20 +1,20 @@
-1 --> 2 --> 3 --> 4 --> 5 --> null
+// 1 --> 2 --> 3 --> 4 --> 5 --> null
 
-let singleLinkedList = {
-    head: {
-        value: 1,
-        next: {
-            value: 2,
-            next: {
-                value: 3,
-                next: {
-                    value: 4,
-                    next: null
-                }
-            }
-        }
-    }
-}
+// let singleLinkedList = {
+//     head: {
+//         value: 1,
+//         next: {
+//             value: 2,
+//             next: {
+//                 value: 3,
+//                 next: {
+//                     value: 4,
+//                     next: null
+//                 }
+//             }
+//         }
+//     }
+// }
 
 class Node {
     constructor(value){ //generar un nodo
@@ -29,9 +29,33 @@ class MySinglyListedList {
             value: value,
             next: null, //el null ya genera un nuevo nodo
         }
+        //tail y head apuntan a la misma direccion de memoria
+		// y si cambia UNA PROPIEDAD de uno, cambiará en el otro
         this.tail = this.head; //cola apuntada a la cabeza o null
-
         this.length = 1; //contar cuantos nodos
+    }
+    //insertar al final
+	//Esto funciona ya que en el contructor hay una referencia
+	//cambiamos UNA PROPIEDAD del tail entonces también cambia en el head
+    append(value){
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++
+        /*
+		Entonces ahora tail y ese nodo que se agregó
+		el next del head es el next de ese nodo que se agregó
+		Ambos next estan referenciados, COMO AL INICIO
+		entonces cuando creemos otro nodo simplemente se repetirá el proceso
+		*/
+        return this;
+    }
+    preppend(value){
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++
+        return this;
     }
 }
 
